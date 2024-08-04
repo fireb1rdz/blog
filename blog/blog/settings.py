51731e729ae3b0661f126606833b0c1cf3cfd861
Gustavo <gustavonelsonpaganelli@gmail.com>
2024-08-04 11:22:30 -0300

    updating settings

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "base", "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "base")
+    os.path.join(BASE_DIR, "base", "static")
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "collect")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
